99ebcef492662603470a184874357a0cbaf47880
Adrianndp <adrian_alberto.diaz_poletti@mailbox.tu-dresden.de>
2024-09-22 19:37:36 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Main from "./components/Main";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 const App = () => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Container maxWidth="lg">
         <Typography component={"span"} variant={"body2"}>
           {/* Define Routes */}
